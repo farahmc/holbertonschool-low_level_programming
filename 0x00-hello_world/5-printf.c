ed6b4a8aa902b34fcfc96320e9e6dc10f6200a8e
farahmc <farah.mccurdy@gmail.com>
2022-02-02 20:59:17 -0800

    c program to print a string using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/* Header file contains function definitions for all input output functions */
+#include <stdio.h>
+
+/**
+ *main - Defines main can only be called without any parameters
+ *
+ *Description: prints string to screen
+ *
+ *Return: 0 at the end of the function
+ */
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a piece of art\,"
+return (0);
+
+}
