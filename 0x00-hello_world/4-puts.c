00de9b52b09c53f1f59bd4e288669efe360b124d
farahmc <farah.mccurdy@gmail.com>
2022-02-02 20:05:38 -0800

    fixed code according to betty standards

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,18 @@
+/* Header file contains function definitions for all input output functions */
 #include <stdio.h>
 
-int main (void)
+/*
+ *main - Defines main can only be called without any parameters
+ *
+ *Description - prints string to screen
+ *
+ *Return - 0 at the end of the function
+ */
+
+int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
-  return(0);
+
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+
 }
