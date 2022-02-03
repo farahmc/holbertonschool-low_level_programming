dfe441d560ea2d4904a477f0ed15f1d0e400dd9a
farahmc <farah.mccurdy@gmail.com>
2022-02-02 20:11:17 -0800

    fixed description according to betty conventions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 /* Header file contains function definitions for all input output functions */
 #include <stdio.h>
 
-/*
+/**
  *main - Defines main can only be called without any parameters
  *
- *Description - prints string to screen
+ *Description: prints string to screen
  *
- *Return - 0 at the end of the function
+ *Return: 0 at the end of the function
  */
 
 int main(void)
