067cb5a89d94a14ac344438897f335ae076594c4
farahmc <farah.mccurdy@gmail.com>
2022-02-04 01:26:57 -0800

    program to print all single digit numbers using putchar function only

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - Defines main can only be called without any parameters
+ *
+ *Description: int defines num to print
+ *for writes the character to stdout
+ *where num starts at 0, stops at 9 and ascends by one increment
+ *Return: 0 at the end of the function
+ *
+ */
+
+int main(void)
+{
+
+int num;
+
+num = '0';
+while (num <= '9')
+{
+	putchar(num);
+	if (num < '9')
+	{ putchar (',');
+	  putchar ('\n');
+	}
+	num++;
+}
+return (0);
+}
