c831004cfbdfc629975937750bcb13495737284e
farahmc <farah.mccurdy@gmail.com>
2022-02-03 21:49:22 -0800

    changed char to int

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  *main - Defines main can only be called without any parameters
  *
- *Description: char defines num to print
+ *Description: int defines num to print
  *for writes the character to stdout
  *where num starts at 0, stops at 9 and ascends by one increment
  *Return: 0 at the end of the function
@@ -12,7 +12,7 @@
 
 int main(void)
 {
-char num;
+int num;
 
 for (num = '0'; num <= '9'; num++)
 putchar(num);
