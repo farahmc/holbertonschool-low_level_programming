16468255032d58c29bb8ddd7cf17afac17ac9788
farahmc <farah.mccurdy@gmail.com>
2022-02-03 21:18:34 -0800

    program that displays last digit of random variable and defines if it is zero, more than 5 or less than 6 but not zero

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Defines main can only be called without any parameters
+*
+*Description: int n defines n as random variable
+*lastn defines the last digit of the random variable
+*if function to print the last digit of random variable and define if it is
+*larger than 5, zero or less than 6 and not 0
+*where ch starts at a, stops at z and ascends by one increment
+*
+*Return: 0 at the end of the function
+*/
+
+int main(void)
+{
+int n;
+int lastn;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastn = n % 10;
+
+printf("Last digit of %d is", n);
+
+if (lastn > 5)
+printf(" %d and is greater than 5\n", lastn);
+else if (n == 0)
+printf(" %d and is 0\n", lastn);
+else if (n < 6 != 0)
+printf(" %d and is less than 6 and not 0\n", lastn);
+
+return (0);
+}
