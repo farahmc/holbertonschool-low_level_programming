48c42fcb5dbc88fb738408cafde9be4f02c4e86f
farahmc <farah.mccurdy@gmail.com>
2022-02-03 17:36:52 -0800

    use putchar to display alphabet from a to z using for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Defines main can only be called without any parameters
+ *
+ *Description: char defines ch to print
+ *for writes the character to stdout
+ *where ch starts at a, stops at z and ascends by one increment
+ *
+ *Return: 0 at the end of the function
+ *
+ */
+
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
