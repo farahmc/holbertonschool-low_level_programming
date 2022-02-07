48ba65c67fd89a61a4a2c468182a2d96199aced4
farahmc <farah.mccurdy@gmail.com>
2022-02-07 15:11:20 -0800

    add 0-putchar.c program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main- Defines main can only be called without any parameters
+ *
+ *Description: print the string '_putchar'
+ *
+ *Return: return 0 at the end of the function
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
