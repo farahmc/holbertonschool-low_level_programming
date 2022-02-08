880e881cdcd674359f44709e831d36472ede3b29
farahmc <farah.mccurdy@gmail.com>
2022-02-07 23:38:31 -0800

    removed single bracket around integers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,22 @@
  */
 int print_sign(int n)
 {
-	if (n == '0')
+	int a;
+
+	if (n == 0)
 	{
+		a = 0;
 		_putchar('0');
-		return (0);
 	}
-	else if (n > '0')
+	else if (n > 0)
 	{
+		a = 1;
 		_putchar('+');
-		return (1);
 	}
 	else
 	{
+		a = -1;
 		_putchar('-');
-		return (-1);
 	}
+	return (a);
 }
