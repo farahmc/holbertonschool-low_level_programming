3f61e16d7b312fb54b360419556db2988dba2cb2
farahmc <farah.mccurdy@gmail.com>
2022-02-07 20:30:09 -0800

    added 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_isalpha- checks for letter
+ *@c: is a character
+ *Description: if c is a letter print 1
+ *
+ *Return: 0 if c is not a letter, print 0
+ *
+ */
+int _isalpha(int c)
+{
+	int n;
+
+	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+	{
+		n = 1;
+	}
+	else
+	{
+		n = 0;
+	}
+	return (n);
+}
