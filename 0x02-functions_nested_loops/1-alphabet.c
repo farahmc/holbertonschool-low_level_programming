b3301342a8a08274f70d4f2814a6a5a62f0641ac
farahmc <farah.mccurdy@gmail.com>
2022-02-07 19:06:56 -0800

    fixed to betty code and betty description

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ *print_alphabet- use _putchar to display alphabet string
+ *
+ *Description: called functions in main.h
+ *_putchar to print alphabet a to z
+ *
+ *Return: 0 at the end of the function
+ */
 void print_alphabet(void)
 {
 	char i;
