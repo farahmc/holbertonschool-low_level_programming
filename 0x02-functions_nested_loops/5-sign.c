c63591b88db430725754fce56d6b4a10edd645c8
farahmc <farah.mccurdy@gmail.com>
2022-02-07 21:52:16 -0800

    changed return at end of each if statement

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,22 +11,20 @@
  */
 int print_sign(int n)
 {
-	int a;
 
 	if (n == '0')
 	{
-		a = 0;
 		_putchar('0');
+		return (0);
 	}
 	else if (n > '0')
 	{
-		a = 1;
 		_putchar('+');
+		return (1);
 	}
 	else
 	{
-		a = -1;
 		_putchar('-');
+		return (-1);
 	}
-	return (a);
 }
