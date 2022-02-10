06b0dad51274d89b7648d061f0a9896ef01e1bfc
farahmc <farah.mccurdy@gmail.com>
2022-02-10 01:10:22 -0800

    added 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+void print_triangle(int size)
+{
+	int row;
+	int col;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for(row = 1; row <= size; row++)
+		{
+			for(col = row; col <= size; col++)
+			{
+				_putchar(' ');
+			}
+			for(col = 1; col <= row; col++)
+				{
+					_putchar('#');
+				}
+			_putchar('\n');
+		}
+	}
+}
