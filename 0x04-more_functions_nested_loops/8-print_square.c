84dcf610e075839713f991406277f59cfd2055c3
farahmc <farah.mccurdy@gmail.com>
2022-02-10 00:46:10 -0800

    fixed to betty standards

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,19 +1,28 @@
 #include "main.h"
 
+/**
+ * print_square- print a square
+ * @size: size of square
+ *
+ * Description- print a square on the terminal with a value given by size
+ *
+ * Return: 0 for success
+ */
+
 void print_square(int size)
 {
 	int row;
 	int col;
 
-	if(size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for(row = 0; row < size; row++)
+		for (row = 0; row < size; row++)
 		{
-			for(col = 0; col < size; col++)
+			for (col = 0; col < size; col++)
 			{
 				_putchar('#');
 			}
