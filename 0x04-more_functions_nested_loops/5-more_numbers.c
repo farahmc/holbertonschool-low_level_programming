7d4d55466c14befdc3c23ead8ee158c27f561639
farahmc <farah.mccurdy@gmail.com>
2022-02-09 19:39:16 -0800

    fixed to betty standards, fixed loop to display 10 lines instead of 11

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,23 @@
 #include "main.h"
 
+/**
+ * more_numbers- print 10 rows of numbers 0-14
+ *
+ * Description: print 10 rows of numbers 0-14 using only putchar
+ *
+ * Return: 0 for success
+ */
+
 void more_numbers(void)
 {
 	int n;
 	int lines;
 
-	for (lines = 0; lines <= 10; lines++)
+	for (lines = 0; lines < 10; lines++)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if(n > 9)
+			if (n > 9)
 			{
 				_putchar(n / 10 + '0');
 			}
