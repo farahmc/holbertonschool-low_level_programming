817b6bb5173c766569a70dd619c987e1df2891f2
farahmc <farah.mccurdy@gmail.com>
2022-02-10 01:16:58 -0800

    fixed to betty standards

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * print_triangle- print a triangle
+ * @size: size of triangle (variable)
+ *
+ * Description: print a triangle shape using only _putchar
+ *
+ * Return- 0 is success
+ */
+
 void print_triangle(int size)
 {
 	int row;
@@ -11,16 +20,16 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for(row = 1; row <= size; row++)
+		for (row = 1; row <= size; row++)
 		{
-			for(col = row; col < size; col++)
+			for (col = row; col < size; col++)
 			{
 				_putchar(' ');
 			}
-			for(col = 1; col <= row; col++)
-				{
-					_putchar('#');
-				}
+			for (col = 1; col <= row; col++)
+			{
+				_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
