f98260fb65ac8552b8e166ef51cbf4bdc6db8f78
farahmc <farah.mccurdy@gmail.com>
2022-02-10 01:32:31 -0800

    fixed to betty standards

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,36 @@
 #include "main.h"
 
+/**
+ * print_diagonal- print a diagonal shape
+ * @n: random variable
+ *
+ * Description: nested loops to print a diagonal image
+ * using only putchar
+ *
+ * Return: 0 is success
+ */
+
 void print_diagonal(int n)
 {
 	int row;
 	int col;
 
-	if(n <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for(row = 0; row < n; row++)
+		for (row = 0; row < n; row++)
 		{
-			for(col = 0; col < n; col++)
+			for (col = 0; col < n; col++)
 			{
-				if(col == row)
+				if (col == row)
 				{
 					_putchar(92);
 					_putchar('\n');
 				}
-				else if(col < row)
+				else if (col < row)
 				{
 					_putchar(' ');
 				}
