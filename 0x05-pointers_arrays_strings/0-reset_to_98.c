1fbcd37d9a3a6ffc3f2ec66a17f165e48c047be4
farahmc <farah.mccurdy@gmail.com>
2022-02-13 15:54:19 -0800

    fixed to betty standards

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - reset *n to 98
+ * @n: int called from main function
+ *
+ * Description: reset value of *n to 98
+ *
+ * Return: 0 is success
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
