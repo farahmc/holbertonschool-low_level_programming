ec95aca02e1874457343b78a1748f724df9f5e28
farahmc <farah.mccurdy@gmail.com>
2022-02-13 16:07:38 -0800

    fixed so swap will be for any integer, fixed to betty standards

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,20 @@
 #include "main.h"
 
+/**
+ * swap_int- swap integers
+ * @a: first integers
+ * @b: second integer
+ *
+ * Description: temp to hold *a so *b can call it prior to change
+ *
+ * Return: 0 for success
+ */
+
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
