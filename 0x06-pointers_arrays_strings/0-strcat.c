0a5ea297b8500ed4ed2f3a648deea05cdbbb4e2a
farahmc <farah.mccurdy@gmail.com>
2022-02-16 17:01:23 -0800

    removed some comments

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -25,14 +25,6 @@ char *_strcat(char *dest, char *src)
 
 	while (src[b] != '\0')
 	{
-
-		/**
-		 * so when you say dest[a] = it's at the \0  of
-		 * dest that you are adding the src bc of the previous loop
-		 * src is on the other side of the = bc you are
-		 * adding src to dest
-		 */
-
 		dest[a] = src[b];
 		b = b + 1;
 		a = a + 1;
