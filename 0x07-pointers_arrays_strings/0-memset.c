f262244d64c78dd9dd94b3e4b4b03244938406bf
farahmc <farah.mccurdy@gmail.com>
2022-02-20 16:03:03 -0800

    added file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _memset- fills memory with a constant byte
+ * @s: pointer of memory area
+ * @b: constant byte
+ * @n: first n bytes of memory area filled by b
+ *
+ * Description: while loop to go through memory pointed to by s
+ * while the count is less than n
+ * fill memory area with b
+ *
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int count;
+
+	count = 0;
+	while (count < n)
+	{
+		s[count] = b;
+		a = a + 1;
+	}
+	return (s);
+}
