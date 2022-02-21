467435e3e9697024a1e4a1f595f40ef6c5a651b4
farahmc <farah.mccurdy@gmail.com>
2022-02-20 19:07:20 -0800

    fixed to betty standards

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+/**
+ * _strchr- locates a character in a string
+ * @s: string to search
+ * @c: character to find in string
+ *
+ * Description: loop to go through characters in s
+ * if the character doesn't match c, keep going
+ * else, return from the location in string
+ * if no characters match, return 0
+ *
+ * Return: s if character located, 0 if not located
+ */
 
 char *_strchr(char *s, char c)
 {
@@ -11,11 +23,11 @@ char *_strchr(char *s, char c)
 		if (s[count] != c)
 			s = s + 1;
 		else
-			return(s);
+			return (s);
 	}
 
 	if (s[count] == '\0')
 		return (s);
 	else
-		return(0);
+		return (0);
 }
