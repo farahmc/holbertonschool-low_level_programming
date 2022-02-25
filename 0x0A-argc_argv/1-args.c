05672192eddf5b567314dc5bba9a4f96647879e5
farahmc <farah.mccurdy@gmail.com>
2022-02-24 18:44:33 -0800

    added file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments passed into program
+ * @argc: argument count
+ * @argv: argument vector (unused)
+ *
+ * Description: print the number of arguments passed into program
+ *
+ * Return: 0 is success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
