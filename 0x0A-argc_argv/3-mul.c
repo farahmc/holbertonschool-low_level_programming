67c51eb71d66484a84e8bca68e64c58765d54c57
farahmc <farah.mccurdy@gmail.com>
2022-02-24 19:36:00 -0800

    added file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- multiply 2 numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Description: multiply 2 numbers given on command line and print result
+ * if no arguments received, print error
+ *
+ * Return: 0 is success, if no arguments received, return 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int mul = 1;
+
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			mul = mul * atoi(argv[i]);
+			i = i + 1;
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error \n");
+		return (1);
+	}
+
+	return (0);
+}
