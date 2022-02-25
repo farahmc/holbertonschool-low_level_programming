2f0a0e55788d91ff67c92a332c8b360a3b4f82f2
farahmc <farah.mccurdy@gmail.com>
2022-02-24 18:20:08 -0800

    added directory, readme and 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- print name of program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Description: print name of program, followed by a new line
+ *
+ * Return: 0 is success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
