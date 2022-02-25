03dcd173198e782d57099e75f347f7575bc54316
farahmc <farah.mccurdy@gmail.com>
2022-02-24 21:49:30 -0800

    reversed if condition

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,7 +17,13 @@ int main(int argc, char *argv[])
 	int i = 1;
 	int mul = 1;
 
-	if (argc > 2)
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	else
 	{
 		while (i < argc)
 		{
@@ -26,11 +32,5 @@ int main(int argc, char *argv[])
 		}
 		printf("%d\n", mul);
 	}
-	else
-	{
-		printf("Error \n");
-		return (1);
-	}
-
 	return (0);
 }
