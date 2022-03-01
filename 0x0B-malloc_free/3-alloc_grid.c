cf73f113891d15c51a2491b79a4f64d043e4733d
farahmc <farah.mccurdy@gmail.com>
2022-03-01 03:18:09 -0800

    fixed to betty

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * alloc_grid- return a pointer to a 2d array of integers
+ * @height: row of integers
+ * @width: column of integers
+ *
+ * Description: set memory allocation of height
+ * loop to go through rows
+ * set memory allocation of width
+ * free space if null byte returned
+ * loop to go through columns and display grid
+ *
+ * Return: null if width or height is 0 or negative
+ * null if null byte in array
+ * array if success
+ */
+
 int **alloc_grid(int width, int height)
 {
 	int **array;
@@ -25,7 +41,7 @@ int **alloc_grid(int width, int height)
 			while (row < height)
 			{
 				free(array[row]);
-				row ++;
+				row = row + 1;
 			}
 			free(array);
 			return (NULL);
@@ -34,9 +50,9 @@ int **alloc_grid(int width, int height)
 		while (col < width)
 		{
 			array[row][col] = 0;
-			col++;
+			col = col + 1;
 		}
-		row++;
+		row = row + 1;
 	}
 
 	return (array);
