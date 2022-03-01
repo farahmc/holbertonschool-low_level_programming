83d74cfb838ca4121e1996935d9015123f4538e3
farahmc <farah.mccurdy@gmail.com>
2022-03-01 01:16:26 -0800

    went back to arrays

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,7 @@
  * @s: string to be counted
  *
  * Description: loop to count number of characters in string
- * that is lcated by a pointer
+ * that is located by a pointer
  *
  * Return: number of characters in string
  */
@@ -22,30 +22,14 @@ int _strlen(char *s)
 	return (len);
 }
 
-/**
- * str_concat- concatenates 2 strings
- * @s1: first string
- * @s2: string to append to s1
- *
- * Description: find length of s1 + s2
- * allocate memory to length of s1 + s2 + 1 to accomodate null byte
- * loop through s1 and s2, assigning to concat as a temp hold
- *
- * Return: Null if null bytes in s1 or s2
- * Null if concat is null
- * concatenated string from beginning if chars present
- */
-
 char *str_concat(char *s1, char *s2)
 {
 	int len;
+	int i = 0;
 	char *concat;
 	char *beg;
 
-	if (s1 == NULL)
-		return (NULL);
-
-	if (s2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 
 	len = _strlen(s1) + _strlen(s2);
@@ -55,19 +39,19 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	beg = concat;
-	while (*s1 != '\0')
+	while (s1[i] != '\0')
 	{
-		*concat = *s1;
-		*concat++;
-		*s1++;
+		concat[i] = s1[i];
+		concat = concat + 1;
+		i = i + 1;
 	}
-	while (*s2 != '\0')
+	while (s2[i] != '\0')
 	{
-		*concat = *s2;
-		*concat++;
-		*s2++;
+		concat[i] = s2[i];
+		concat = concat + 1;
+		i = i + 1;
 	}
 	*concat = '\0';
 
-	return (beg);
+	return(beg);
 }
