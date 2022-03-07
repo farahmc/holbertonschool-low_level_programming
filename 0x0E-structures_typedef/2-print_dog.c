eafc68cf3d0acb7e223a550f25dd15a1e908a52d
farahmc <farah.mccurdy@gmail.com>
2022-03-06 19:52:36 -0800

    fixed logic

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,21 +14,18 @@
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		if (d->name == NULL)
-			printf("Name: (nil)");
-		else
-			printf("Name: %s\n", d->name);
+	if (d == NULL)
+		return;
 
-		if (d->age <= 0)
-			printf("Age: (nil)");
-		else
-			printf("Age: %f\n", d->age);
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
 
-		if (d->owner == NULL)
-			printf("Owner: (nil)");
-		else
-			printf("Owner: %s\n", d->owner);
-	}
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
