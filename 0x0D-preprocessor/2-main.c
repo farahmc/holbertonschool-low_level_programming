43518bf7bada1441b940d5d84c6be54408b23afc
farahmc <farah.mccurdy@gmail.com>
2022-03-06 17:49:10 -0800

    added comments

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main- print the name of the file the exe was compiled from
+ *
+ * Description: print the name of the file the exe was compiled from
+ * using __FILE__ macro
+ *
+ * Return: 0 is success
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
