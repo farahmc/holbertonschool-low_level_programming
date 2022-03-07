d9d82cbc7976e7b56dc0bdf3582f5c1ccd6f9f01
farahmc <farah.mccurdy@gmail.com>
2022-03-06 18:03:14 -0800

    added comments

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_
 #define _DOG_
 
+/**
+ * struct dog - new type struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: struct dog
+ */
+
 struct dog
 {
 	char *name;
