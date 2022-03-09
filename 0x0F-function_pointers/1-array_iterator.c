3b0188d9d8148b0582441c41e1244e89810f9a38
farahmc <farah.mccurdy@gmail.com>
2022-03-08 16:57:09 -0800

    added file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i = i + 1;
+	}
+}
