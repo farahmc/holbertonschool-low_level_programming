f3af869f7dda3e434a71ec10fba6af6b6d099ed4
farahmc <farah.mccurdy@gmail.com>
2022-03-08 17:45:07 -0800

    added description, returned array value instead of count value

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * int_index- search for an integer
+ * @array: array of integers
+ * @size: number of elements in array
+ * @cmp: pointer to function comparing values
+ *
+ * Description: while i is less than size,
+ * loop through array, if cmp does not return 0
+ * return the element
+ *
+ * Return: if size is less than 0, return -1
+ * if no element matches, return -1
+ * else return the index of the first element for which
+ * cmp does not return 0
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
@@ -19,5 +35,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		}
 		i++;
 	}
-	return(i);
+	return (array[i]);
 }
