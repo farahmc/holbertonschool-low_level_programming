8a1c0031fc1f06554837041099a7250fa05aa628
farahmc <farah.mccurdy@gmail.com>
2022-03-09 02:36:48 -0800

    added file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int calc;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][0] != '+' || argv[2][0] != '-' || argv[2][0] != '*'
+	    || argv[2][0] != '/' || argv[2][0] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	calc = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", calc);
+
+	return (0);
+}
