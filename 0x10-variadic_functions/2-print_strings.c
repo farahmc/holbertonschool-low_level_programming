f1c68a7e6b825798a8edd317547f99a4e25581df
farahmc <farah.mccurdy@gmail.com>
2022-03-09 17:41:45 -0800

    removed new line after nil

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		string = va_arg(args, char *);
 		if (string == NULL)
-			printf("(nil)\n");
+			printf("(nil)");
 
 		printf("%s", string);
 
