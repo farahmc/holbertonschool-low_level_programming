685fad2eec7c5aec532b709fd91fdcb6bc2073be
farahmc <farah.mccurdy@gmail.com>
2022-03-09 18:17:51 -0800

    moved if separator is not null condition to last if loop

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,16 +15,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	va_list args;
 
-	if (separator == NULL)
-		return;
-
 	va_start(args, n);
 
 	while (i < n)
 	{
 		printf("%d", va_arg(args, unsigned int));
 
-		if (i < (n - 1))
+		if (i < (n - 1) && (separator != NULL))
 			printf("%s", separator);
 
 		i++;
