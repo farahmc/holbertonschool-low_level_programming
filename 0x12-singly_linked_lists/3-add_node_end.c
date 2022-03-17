64d5bbdaeed20d40d7f3c5e3c7640bcb14f4345a
farahmc <farah.mccurdy@gmail.com>
2022-03-16 22:16:26 -0700

    free newNode if null

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -44,7 +44,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	newNode->str = strdup(str);
 
 	if (newNode->str == NULL)
+	{
+		free (newNode);
 		return (NULL);
+	}
 
 	newNode->len = _strlen(newNode->str);
 	newNode->next = NULL;
