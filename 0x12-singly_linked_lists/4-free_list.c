327bcc0f5f38e2b4602ce5cf8046a623ed6bce22
farahmc <farah.mccurdy@gmail.com>
2022-03-17 02:37:54 -0700

    fixed to betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_list- free a list
+ * @head: pointer to the first element of a linked lise
+ *
+ * Description: free a list with no possible leaks
+ *
+ * Return: void
+ */
+
 void free_list(list_t *head)
 {
 	free(head);
