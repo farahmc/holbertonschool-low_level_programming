0a25768c7f8867d8bd048c44695b76ae524a17e0
farahmc <farah.mccurdy@gmail.com>
2022-03-17 02:46:44 -0700

    freed all malloc'd spaces

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -13,5 +13,14 @@
 
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *start = head;
+	list_t *temp; /*temp is always used to manipulate list*/
+
+	while (start != NULL)
+	{
+		temp = start;
+		start = start->next;
+		free(temp->str);
+		free(temp);
+	}
 }
