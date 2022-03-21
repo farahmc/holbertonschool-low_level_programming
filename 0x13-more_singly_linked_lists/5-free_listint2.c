716e037f5b551a9369deb4be30f949a246c16726
farahmc <farah.mccurdy@gmail.com>
2022-03-21 04:37:22 -0700

    added file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list
+ * @head: pointer to the first element of a linked lise
+ *
+ * Description: free a list with no possible leaks
+ * set head to NULL
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *start, *temp;
+
+	start = *head;
+	while (start != NULL)
+	{
+		temp = start;
+		start = start->next;
+		free(temp);
+		*head = NULL;
+	}
+
+}
