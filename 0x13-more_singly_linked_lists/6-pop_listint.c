fc24d7d721209a707863c2ef5a39660a9c261209
farahmc <farah.mccurdy@gmail.com>
2022-03-21 16:00:34 -0700

    fixed betty and logic in first if condition

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,13 +3,22 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * pop_listint - delete the head node
+ * @head: pointer to a pointer to the first element of a linked lise
+ *
+ * Description: delete the head node of a linked list
+ *
+ * Return: Null if linked list is empty, else return the data in head node
+ */
+
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 	int n;
 
-	if (head == NULL)
-		return(0);
+	if (*head == NULL)
+		return (0);
 
 	temp = *head;
 	*head = temp->next;
