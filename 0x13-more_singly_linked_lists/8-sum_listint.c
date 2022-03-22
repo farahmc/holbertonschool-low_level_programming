20f6dade32b1847823cf8400be370a83717636ce
farahmc <farah.mccurdy@gmail.com>
2022-03-21 17:19:12 -0700

    added file

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - return sum of data
+ * @head: pointer to the first element of a linked list
+ *
+ * Description: add all values of data in nodes of a linked list
+ *
+ * Return: 0 if list is empty, else return sum of data
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
