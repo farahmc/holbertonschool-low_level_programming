3cf7fc328a4aaf96618b05ff30ce0e4100feb086
farahmc <farah.mccurdy@gmail.com>
2022-03-24 04:16:10 -0700

    added file

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_bit - set a bit to 1 at a given index
+ * @n: the given number
+ * @index: the position to set a bit to 1
+ *
+ * Description: shift bits left index number of times
+ * then set bit to 1
+ *
+ * Return: 1 if bit is changed, else -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n | (1 << index);
+	return (1);
+
+	if (index > *n)
+		return (-1);
+
+}
