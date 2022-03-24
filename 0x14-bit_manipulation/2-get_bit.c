c249fc75762f0fe322693405405079f06db585e8
farahmc <farah.mccurdy@gmail.com>
2022-03-24 03:57:13 -0700

    added file

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - return the value of a bit at given position
+ * @n: the number
+ * @index: the position of which to check the value
+ *
+ * Description: interate through bits by right shifting
+ * if loop position is equal to index, return the value
+
+ * Return: value of the bit at the given index, or -1 if error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (index > 63)
+		return (-1);
+
+	while (n > 0)
+	{
+		if (i == index)
+			return (n & 1);
+		n = n >> 1;
+		i++;
+	}
+
+	return (0);
+}
