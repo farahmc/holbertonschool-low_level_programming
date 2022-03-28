fb2ccebc4aab2aa1fcb77e960ef35b7c263c02cb
farahmc <farah.mccurdy@gmail.com>
2022-03-27 21:52:16 -0700

    fixed permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -45,7 +45,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	open_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	open_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (open_file == -1)
 		return (-1);
 
