3e4beda1f8aafd4896c8c6470e47acfa0b691baf
farahmc <farah.mccurdy@gmail.com>
2022-03-27 21:29:05 -0700

    added a small function for strlen and used in main

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,6 +7,27 @@
 #include "main.h"
 
 /**
+ * _strlen- counts number of characters in string
+ * @s: string to be counted
+ *
+ * Description: loop to count number of characters in string
+ * that is lcated by a pointer
+ *
+ * Return: number of characters in string
+ */
+
+int _strlen(char *s)
+{
+	int numofchar;
+
+	for (numofchar = 0; *s != '\0'; s++)
+		numofchar++;
+
+	return (numofchar);
+}
+
+
+/**
  * create_file - create a file
  * @filename: name of file
  * @text_content: content to write in file
@@ -19,23 +40,17 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int open_file, write_file, i;
+	int open_file, write_file, length;
 
 	if (filename == NULL)
 		return (-1);
 
-	open_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	open_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
 
-	i = 0;
-	if (text_content != NULL)
-	{
-		while (text_content[i] != '\0')
-			i++;
-	}
+	length = _strlen(text_content);
+	write_file = write(open_file, text_content, length);
 
-	write_file = write(open_file, text_content, i);
-
-	if (open_file == -1 || write_file == 1)
+	if (open_file == -1 || write_file == -1)
 		return (-1);
 
 	close(open_file);
