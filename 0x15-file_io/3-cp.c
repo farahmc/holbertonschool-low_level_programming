39851ed7eec278c58c56c0313c95f59bf9ef68b9
farahmc <farah.mccurdy@gmail.com>
2022-03-28 20:26:45 -0700

    added file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,12 +19,13 @@
 
 int main(int argc, char *argv[])
 {
-	int source, dest;
+	int source, dest, _read, _write;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-			exit(97);
+		exit(97);
 	}
 
 	source = open(argv[1], O_RDONLY);
@@ -41,8 +42,42 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
+	_read = 1024;
+	while (_read == 1024)
+	{
+		_read = read(source, buffer, 1024);
 
-	close(source);
-	close(dest);
+		if (_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close(source);
+			exit(98);
+		}
+
+		_write = write(dest, buffer, _read);
+		if (_write == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(dest);
+			exit(99);
+		}
+	}
+
+	if (close(source) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source);
+		exit(100);
+	}
+	else
+		close(source);
+
+	if (close(dest) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest);
+		exit(100);
+	}
+	else
+		close(dest);
+
 	return (0);
 }
