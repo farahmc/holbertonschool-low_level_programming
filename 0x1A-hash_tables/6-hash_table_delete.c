6fad65060655d9897f5958ec7fc328b59cc170b5
Farah <farah.mccurdy@gmail.com>
2022-05-27 04:36:22 -0700

    add: file

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete a hash table
+ * @ht: hash table to delete
+ *
+ * Description: delete a hash table, free all allocated memory
+ *
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i = 0;
+
+	while (i < ht->size)
+	{
+		hash_node_t *temp = ht->array[i];
+
+		if (temp != NULL)
+		{
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
