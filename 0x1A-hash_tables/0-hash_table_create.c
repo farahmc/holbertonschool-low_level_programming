115f7d100019dee4cb7ecee7d84dd8507d2418f7
Farah <farah.mccurdy@gmail.com>
2022-05-26 17:44:45 -0700

    add: early return if size is null

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,16 +15,25 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned int i = 0;
 
+	if (size == 0)
+		return (NULL);
+	
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t) * size);
 	if (table->array == NULL)
+	{
+		free(table->array);
 		return (NULL);
+	}
 
-	while (i < size)
+	while (i < table->size)
 	{
 		table->array[i] = NULL;
 		i++;
