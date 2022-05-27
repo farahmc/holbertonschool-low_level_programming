8fa9301f630808c15ec099092c85d26d25841dee
Farah <farah.mccurdy@gmail.com>
2022-05-27 04:19:11 -0700

    add: file

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print a hash table
+ * @ht: the hash table to print
+ *
+ * Description: print the hash table in order they appear
+ * in the array
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i = 0;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	while (i < ht->size)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			printf("'%s': '%s', ", temp->key, temp->value);
+			temp = temp->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
